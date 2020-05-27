a61b900dbe4e69e371ab05da5c9e2590fa1271be
Gillen Brown <gillenb@umich.edu>
2020-05-27 17:27:57 -0400

    revamped ryon comparison

--- legus_sizes/ryon_comparison.py
+++ legus_sizes/ryon_comparison.py
@@ -45,7 +45,6 @@ for key in catalogs:
     if catalogs[key] is None:
         raise RuntimeError(f"The {key} catalog has not been created!")
 
-
 # ======================================================================================
 #
 # Matching catalogs
@@ -54,17 +53,24 @@ for key in catalogs:
 # Here Ryon's catalogs combine the two fields for these two galaxies. I won't do that,
 # as I want to compare the different fields separately, but I do need to match to the
 # IDs used in the Ryon catalogs
-catalogs["ngc1313-e"]["ID"] = [f"{i}-e" for i in catalogs["ngc1313-e"]["ID"]]
-catalogs["ngc1313-w"]["ID"] = [f"{i}-w" for i in catalogs["ngc1313-w"]["ID"]]
-catalogs["ngc628-e"]["ID"] = [f"{i}-e" for i in catalogs["ngc628-e"]["ID"]]
-catalogs["ngc628-c"]["ID"] = [f"{i}-c" for i in catalogs["ngc628-c"]["ID"]]
+for name, cat in catalogs.items():
+    cat["ID"] = [f"{i}{name[-2:]}" for i in cat["ID"]]
 
 # Then match the tables together. Using the inner join type is the strict intersection
 # where the matched keys must match exactly
-m_1313_e = table.join(catalogs["ngc1313-e"], ryon_1313, join_type="inner", keys="ID")
-m_1313_w = table.join(catalogs["ngc1313-w"], ryon_1313, join_type="inner", keys="ID")
-m_628_e = table.join(catalogs["ngc628-e"], ryon_628, join_type="inner", keys="ID")
-m_628_c = table.join(catalogs["ngc628-c"], ryon_628, join_type="inner", keys="ID")
+matches = dict()
+matches["ngc1313-e"] = table.join(
+    catalogs["ngc1313-e"], ryon_1313, join_type="inner", keys="ID"
+)
+matches["ngc1313-w"] = table.join(
+    catalogs["ngc1313-w"], ryon_1313, join_type="inner", keys="ID"
+)
+matches["ngc628-e"] = table.join(
+    catalogs["ngc628-e"], ryon_628, join_type="inner", keys="ID"
+)
+matches["ngc628-c"] = table.join(
+    catalogs["ngc628-c"], ryon_628, join_type="inner", keys="ID"
+)
 
 # ======================================================================================
 #
@@ -73,80 +79,86 @@ m_628_c = table.join(catalogs["ngc628-c"], ryon_628, join_type="inner", keys="ID
 # ======================================================================================
 limits = 0.1, 100
 # First we'll make a straight comparison
-fig, axs = bpl.subplots(figsize=[14, 7], ncols=2)
-for ax, method in zip(axs, ["Galfit", "CI"]):
-    ax.errorbar(
-        m_1313_e[f"r_eff_{method}"],
-        m_1313_e["effective_radius_pc"],
-        xerr=[m_1313_e[f"e_r_eff-_{method}"], m_1313_e[f"e_r_eff+_{method}"]],
-        label="NGC1313-e",
-    )
-    ax.errorbar(
-        m_1313_w[f"r_eff_{method}"],
-        m_1313_w["effective_radius_pc"],
-        xerr=[m_1313_w[f"e_r_eff-_{method}"], m_1313_w[f"e_r_eff+_{method}"]],
-        label="NGC1313-w",
-    )
-    ax.errorbar(
-        m_628_e[f"r_eff_{method}"],
-        m_628_e["effective_radius_pc"],
-        xerr=[m_628_e[f"e_r_eff-_{method}"], m_628_e[f"e_r_eff+_{method}"]],
-        label="NGC628-e",
-    )
-    ax.errorbar(
-        m_628_c[f"r_eff_{method}"],
-        m_628_c["effective_radius_pc"],
-        xerr=[m_628_c[f"e_r_eff-_{method}"], m_628_c[f"e_r_eff+_{method}"]],
-        label="NGC628-c",
-    )
-    ax.set_yscale("log")
-    ax.set_xscale("log")
+fig, ax = bpl.subplots(figsize=[7, 7])
+for idx, (field, cat) in enumerate(matches.items()):
+    ryon_eta = cat["Eta"]
+    my_eta = cat["power_law_slope"]
+    ryon_mask = ryon_eta > 1.3
+    my_mask = my_eta > 1.3
+    mask = np.logical_and(ryon_mask, my_mask)
 
-    ax.set_limits(*limits, *limits)
-    ax.plot(limits, limits, c=bpl.almost_black, lw=1, zorder=0)
-    ax.equal_scale()
-    ax.legend(loc=4)
-    ax.add_labels(
-        "Cluster $R_{eff}$ [pc] - Ryon+ 2017", "Cluster $R_{eff}$ [pc] - Me", method
+    c = bpl.color_cycle[idx]
+
+    ax.errorbar(
+        cat["r_eff_Galfit"][mask],
+        cat["effective_radius_pc"][mask],
+        xerr=[cat[f"e_r_eff-_Galfit"][mask], cat[f"e_r_eff+_Galfit"][mask]],
+        markerfacecolor=c,
+        markeredgecolor=c,
+        ecolor=c,
+        label=field,
+        elinewidth=2,
+        zorder=2,
     )
+
+    ax.errorbar(
+        cat["r_eff_Galfit"][~mask],
+        cat["effective_radius_pc"][~mask],
+        xerr=[cat[f"e_r_eff-_Galfit"][~mask], cat[f"e_r_eff+_Galfit"][~mask]],
+        markerfacecolor="white",
+        markeredgecolor=c,
+        ecolor=c,
+        elinewidth=0.5,
+        zorder=1,
+    )
+ax.set_yscale("log")
+ax.set_xscale("log")
+
+ax.set_limits(*limits, *limits)
+ax.plot(limits, limits, c=bpl.almost_black, lw=1, zorder=0)
+ax.equal_scale()
+ax.legend(loc=4)
+ax.add_labels("Cluster $R_{eff}$ [pc] - Ryon+ 2017", "Cluster $R_{eff}$ [pc] - Me")
 fig.savefig("comparison_plot.png")
 
 # Then a ratio comparison
-fig, axs = bpl.subplots(figsize=[14, 7], ncols=2)
-for ax, method in zip(axs, ["Galfit", "CI"]):
-    ax.errorbar(
-        m_1313_e[f"r_eff_{method}"],
-        m_1313_e["effective_radius_pc"] / m_1313_e[f"r_eff_{method}"],
-        xerr=[m_1313_e[f"e_r_eff-_{method}"], m_1313_e[f"e_r_eff+_{method}"]],
-        label="NGC1313-e",
-    )
-    ax.errorbar(
-        m_1313_w[f"r_eff_{method}"],
-        m_1313_w["effective_radius_pc"] / m_1313_w[f"r_eff_{method}"],
-        xerr=[m_1313_w[f"e_r_eff-_{method}"], m_1313_w[f"e_r_eff+_{method}"]],
-        label="NGC1313-w",
-    )
-    ax.errorbar(
-        m_628_e[f"r_eff_{method}"],
-        m_628_e["effective_radius_pc"] / m_628_e[f"r_eff_{method}"],
-        xerr=[m_628_e[f"e_r_eff-_{method}"], m_628_e[f"e_r_eff+_{method}"]],
-        label="NGC628-e",
-    )
-    ax.errorbar(
-        m_628_c[f"r_eff_{method}"],
-        m_628_c["effective_radius_pc"] / m_628_c[f"r_eff_{method}"],
-        xerr=[m_628_c[f"e_r_eff-_{method}"], m_628_c[f"e_r_eff+_{method}"]],
-        label="NGC628-c",
-    )
-    ax.set_yscale("log")
-    ax.set_xscale("log")
+fig, ax = bpl.subplots(figsize=[7, 7])
+for idx, (field, cat) in enumerate(matches.items()):
+    ryon_eta = cat["Eta"]
+    my_eta = cat["power_law_slope"]
+    ryon_mask = ryon_eta > 1.3
+    my_mask = my_eta > 1.3
+    mask = np.logical_and(ryon_mask, my_mask)
 
-    limits = 0.1, 30
-    ax.set_limits(*limits, 0.1, 10)
-    ax.plot(limits, [1, 1], c=bpl.almost_black, lw=1, zorder=0)
-    ax.legend()
-    ax.add_labels(
-        "Cluster $R_{eff}$ [pc] - Ryon+ 2017", "$R_{eff, Me} / R_{eff, Ryon}$", method
+    c = bpl.color_cycle[idx]
+
+    ax.errorbar(
+        cat["r_eff_Galfit"][mask],
+        cat["effective_radius_pc"][mask] / cat["r_eff_Galfit"][mask],
+        xerr=[cat[f"e_r_eff-_Galfit"][mask], cat[f"e_r_eff+_Galfit"][mask]],
+        markerfacecolor=c,
+        markeredgecolor=c,
+        ecolor=c,
+        label=field,
+        elinewidth=2,
+        zorder=2,
     )
 
+    ax.errorbar(
+        cat["r_eff_Galfit"][~mask],
+        cat["effective_radius_pc"][~mask] / cat["r_eff_Galfit"][~mask],
+        xerr=[cat[f"e_r_eff-_Galfit"][~mask], cat[f"e_r_eff+_Galfit"][~mask]],
+        markerfacecolor="white",
+        markeredgecolor=c,
+        ecolor=c,
+        elinewidth=0.5,
+        zorder=1,
+    )
+ax.set_yscale("log")
+ax.set_xscale("log")
+
+ax.set_limits(*limits, 0.1, 10)
+ax.plot(limits, [1, 1], c=bpl.almost_black, lw=1, zorder=0)
+ax.legend()
+ax.add_labels("Cluster $R_{eff}$ [pc] - Ryon+ 2017", "$R_{eff, Me} / R_{eff, Ryon}$")
 fig.savefig("comparison_ratio_plot.png")
