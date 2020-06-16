c7f768c3a2dfe7990725ecf0a9fda35445ee350e
Gillen Brown <gillenb@umich.edu>
2020-06-16 17:30:35 -0400

    added copying scrip
    This will make it easy to copy the data over once it runs on shangrila

---
+++ copy_from_shangrila.sh
@@ -0,0 +1,1 @@
+rsync -av --exclude '*drc.fits' --exclude '*tar.gz' --exclude '*padagb-mwext-avgapcor*' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/legus/*" /Users/gillenb/google_drive/research/legus/data/
