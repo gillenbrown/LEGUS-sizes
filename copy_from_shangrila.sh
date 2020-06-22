267a0c175c855ac81d4896d4899e494c4afd8cbb
Gillen Brown <gillenb@umich.edu>
2020-06-22 15:45:49 -0400

    copy best fit plots before bootstrapping plots

--- copy_from_shangrila.sh
+++ copy_from_shangrila.sh
@@ -1,3 +1,4 @@
 rsync -av gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/plots/*" /Users/gillenb/code/legus_sizes/plots/
 rsync -av --exclude '*.fits' --exclude '*tar.gz' --exclude '*.png' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/legus/*" /Users/gillenb/google_drive/research/legus/data/
+rsync -av --exclude '*drc.fits' --exclude '*sci.fits' --exclude '*sigma_electrons.fits' --exclude '*tar.gz' --exclude '*bootstrapping*' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/legus/*" /Users/gillenb/google_drive/research/legus/data/
 rsync -av --exclude '*drc.fits' --exclude '*sci.fits' --exclude '*sigma_electrons.fits' --exclude '*tar.gz' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/legus/*" /Users/gillenb/google_drive/research/legus/data/
