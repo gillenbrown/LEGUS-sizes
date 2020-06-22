82c4f6b4f93a23937619309e83f97ddbe49cf3a8
Gillen Brown <gillenb@umich.edu>
2020-06-22 15:17:07 -0400

    copy plots and images last
    This gets the catalogs copied over quicker

--- copy_from_shangrila.sh
+++ copy_from_shangrila.sh
@@ -1,2 +1,3 @@
 rsync -av gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/plots/*" /Users/gillenb/code/legus_sizes/plots/
+rsync -av --exclude '*.fits' --exclude '*tar.gz' --exclude '*.png' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/legus/*" /Users/gillenb/google_drive/research/legus/data/
 rsync -av --exclude '*drc.fits' --exclude '*sci.fits' --exclude '*sigma_electrons.fits' --exclude '*tar.gz' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/legus/*" /Users/gillenb/google_drive/research/legus/data/
