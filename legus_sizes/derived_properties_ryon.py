d950ae26aab4ae27953b5fb0fe6429d0feffc1b0
Gillen Brown <gillenb@umich.edu>
2020-06-25 15:00:13 -0400

    added rmax properties to Ryon comparison
    this allows me to compare the two in these galaxies

--- legus_sizes/derived_properties_ryon.py
+++ legus_sizes/derived_properties_ryon.py
@@ -83,6 +83,22 @@ def eff_profile_r_eff_no_rmax(eta, a, q):
     return ellipticy_correction(q) * a * np.sqrt(2 ** (1 / (eta - 1)) - 1)
 
 
+def eff_profile_r_eff_with_rmax(eta, a, q, rmax):
+    """
+    Calculate the effective radius of an EFF profile, assuming a maximum radius.
+
+    :param eta: Power law slope of the EFF profile
+    :param a: Scale radius of the EFF profile, in any units.
+    :param q: Axis ratio of the profile
+    :param rmax: Maximum radius for the profile, in the same units as a.
+    :return: Effective radius, in the same units as a and rmax
+    """
+    # This is such an ugly formula, put it in a few steps
+    term_1 = 1 + (1 + (rmax / a) ** 2) ** (1 - eta)
+    term_2 = (0.5 * (term_1)) ** (1 / (1 - eta)) - 1
+    return ellipticy_correction(q) * a * np.sqrt(term_2)
+
+
 # ======================================================================================
 #
 # Then add these quantities to the table
@@ -140,6 +156,69 @@ for row in fits_catalog:
 
     row["r_eff_pc_no_rmax_e+_with_dist"] = high_e
     row["r_eff_pc_no_rmax_e-_with_dist"] = low_e
+
+# ======================================================================================
+#
+# Add the quantities with a maximum radius
+#
+# ======================================================================================
+# add the columns we want to the table
+fits_catalog["r_eff_pixels_rmax_15pix_e+"] = -99.9
+fits_catalog["r_eff_pixels_rmax_15pix_e-"] = -99.9
+fits_catalog["r_eff_pc_rmax_15pix_best"] = -99.9
+fits_catalog["r_eff_pc_rmax_15pix_e+"] = -99.9
+fits_catalog["r_eff_pc_rmax_15pix_e-"] = -99.9
+fits_catalog["r_eff_pc_rmax_15pix_e+_with_dist"] = -99.9
+fits_catalog["r_eff_pc_rmax_15pix_e-_with_dist"] = -99.9
+
+# first calculate the best fit value
+fits_catalog["r_eff_pixels_rmax_15pix_best"] = eff_profile_r_eff_with_rmax(
+    fits_catalog["power_law_slope_best"],
+    fits_catalog["scale_radius_pixels_best"],
+    fits_catalog["axis_ratio_best"],
+    15,  # size of the box
+)
+
+# calculate the distribution of all effective radii in each case
+for row in fits_catalog:
+    # Calculate the effective radius in pixels for all bootstrapping iterations
+    all_r_eff_pixels = eff_profile_r_eff_with_rmax(
+        row["power_law_slope"],
+        row["scale_radius_pixels"],
+        row["axis_ratio"],
+        15,  # size of the box
+    )
+    # then get the 1 sigma error range of that
+    lo, hi = np.percentile(all_r_eff_pixels, [15.85, 84.15])
+    # subtract the middle to get the error range. If the best fit it outside the error
+    # range, make the error in that direction zero.
+    row["r_eff_pixels_rmax_15pix_e+"] = max(hi - row["r_eff_pixels_rmax_15pix_best"], 0)
+    row["r_eff_pixels_rmax_15pix_e-"] = max(row["r_eff_pixels_rmax_15pix_best"] - lo, 0)
+
+    # Then we can convert to pc. First do it without including distance errors
+    best, low_e, high_e = utils.pixels_to_pc_with_errors(
+        final_catalog_path.parent.parent,
+        row["r_eff_pixels_rmax_15pix_best"],
+        row["r_eff_pixels_rmax_15pix_e-"],
+        row["r_eff_pixels_rmax_15pix_e+"],
+        include_distance_err=False,
+    )
+
+    row["r_eff_pc_rmax_15pix_best"] = best
+    row["r_eff_pc_rmax_15pix_e+"] = high_e
+    row["r_eff_pc_rmax_15pix_e-"] = low_e
+
+    # Then recalculate the errors including the distance errors
+    _, low_e, high_e = utils.pixels_to_pc_with_errors(
+        final_catalog_path.parent.parent,
+        row["r_eff_pixels_rmax_15pix_best"],
+        row["r_eff_pixels_rmax_15pix_e-"],
+        row["r_eff_pixels_rmax_15pix_e+"],
+        include_distance_err=True,
+    )
+
+    row["r_eff_pc_rmax_15pix_e+_with_dist"] = high_e
+    row["r_eff_pc_rmax_15pix_e-_with_dist"] = low_e
 
 # ======================================================================================
 #
