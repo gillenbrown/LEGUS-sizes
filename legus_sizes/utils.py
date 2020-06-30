5f846f85dd222dfd60c0491ba72d136a7961d8c4
Gillen Brown <gillenb@umich.edu>
2020-06-30 17:01:50 -0400

    updates to distances (unfinished)
    I wanted to commit this now before I lose all the distances I worked hard to get

--- legus_sizes/utils.py
+++ legus_sizes/utils.py
@@ -83,34 +83,104 @@ def get_f555w_pixel_scale_pc(home_dir):
 
 
 # https://en.wikipedia.org/wiki/Distance_modulus
-def distance_modulus_to_distance_mpc(distance_modulus):
-    return 10 ** (1 + 0.2 * distance_modulus) / 1e6
+def distance_modulus_distance_and_err(distance_modulus, distance_modulus_error):
+    distance = 0 ** (1 + 0.2 * distance_modulus) / 1e6
+    distance_err = np.log(10) * 0.2 * distance * distance_modulus_error
+    return distance, distance_err
 
 
-def distance_modulus_err_to_distance_err_mpc(distance_modulus, distance_modulus_error):
-    distance = distance_modulus_to_distance_mpc(distance_modulus)
-    return np.log(10) * 0.2 * distance * distance_modulus_error
+def mean_with_error(*args):
+    """
+    Calculate the mean of certain quantities, each with an error.
+
+    :param args: Tuples, each of the (value, error) form
+    :return: tuple with the mean and error
+    """
+    values = [item[0] for item in args]
+    errors = [item[1] for item in args]
+
+    weights = [1 / sigma for sigma in errors]
+    mean = np.average(values, weights=weights)
+
+    # https://stats.stackexchange.com/questions/271643/uncertainty-of-a-weighted-mean-of-uncertain-observations
+    # variance calculated here (assuming all observations are independent)
+    variance = len(values) * (np.sum(weights)) ** -2
+    return mean, np.sqrt(variance)
 
 
-# http://edd.ifa.hawaii.edu/get_results_pgc.php?pgc=12286
+# I separate out the distances Ryon used from my default values, to ensure consistency
+# in the comparison plots
+ryon_distances_and_errs_mpc = {
+    "ngc1313-e": distance_modulus_distance_and_err(28.21, 0.02),  # Jacobs et al. 2009
+    "ngc1313-w": distance_modulus_distance_and_err(28.21, 0.02),  # Jacobs et al. 2009
+    "ngc628-c": distance_modulus_distance_and_err(29.98, 0.28),  # Olivares et al. 2010
+    "ngc628-e": distance_modulus_distance_and_err(29.98, 0.28),  # Olivares et al. 2010
+}
+
+
+distances_and_errs_mpc = {
+    # unless otherwise indicated, distances are from Sabbi et al 2018.
+    "ic4247": (5.11, 0.4),
+    # IC 559 does not have a good distance in Sabbi et al 2018, as it does not have a
+    # full magnitude below the TRGB. But there are only two measured distances,
+    # according to NED. The other is from Tully-Fisher and has no listed error,
+    # so we use Sabbi et al. 2018
+    "ic559": (10.0, 0.9),
+    # For NGC 1313 we use the mean of both fields from  Sabbi et al. 2018
+    "ngc1313-e": mean_with_error((4.2, 0.34), (4.4, 0.35)),
+    "ngc1313-w": mean_with_error((4.2, 0.34), (4.4, 0.35)),
+    # Sabbi et al. 2018 says: A combination of distance, crowding, and metallicity made
+    # the estimate of the TRGB luminosity of the NGC1433 quite uncertain. The only
+    # other available estimates are Tully Fisher, and the modern ones (>1990) are
+    # consistent with Sabbi et al. 2018, so I just use it for consistency.
+    "ngc1433": (9.1, 1.0),
+    "ngc1566": (18, 2),
+    "ngc1705": (5.22, 0.38),
+    "ngc3344": (8.3, 0.7),
+    # Sabbi et al. 2018 says: A combination of distance, crowding, and metallicity made
+    # the estimate of the TRGB luminosity of the NGC3351 quite uncertain. There are many
+    # other distance indicators on NED. Sabbi et sl. 2018 is a bit lower than the
+    # Cepheid distances, for example, but is still roughly consistent.
+    "ngc3351": (9.3, 0.9),
+    "ngc3738": (5.09, 0.40),
+    "ngc4242": (5.3, 0.3),
+    # For NGC 4395 we use the mean of both fields from  Sabbi et al. 2018
+    "ngc4395-n": mean_with_error((4.62, 0.2), (4.41, 0.33)),
+    "ngc4395-s": mean_with_error((4.62, 0.2), (4.41, 0.33)),
+    "ngc4449": (4.01, 0.30),
+    "ngc45": (6.8, 0.5),
+    "ngc4656": (7.9, 0.7),
+    # For the NGC5194/5195 mosaic, we use the mean of the the 5194 NE and SW fields,
+    # as these are not crowded in Sabbi et al 2018
+    "ngc5194-ngc5195-mosaic": mean_with_error((7.2, 0.6), (7.6, 0.6)),
+    "ngc5238": (4.43, 0.34),
+    "ngc5253": (3.32, 0.25),
+    "ngc5474": (6.6, 0.5),
+    "ngc5477": (6.7, 0.5),
+    # For NGC 628 we use the distance from the outer field
+    "ngc628-c": (8.8, 0.7),
+    "ngc628-e": (8.8, 0.7),
+    "ngc6503": (6.3, 0.5),
+    # For NGC 7793 we take the mean of both fields from Sabbi et al. 2018
+    "ngc7793-e": mean_with_error((3.75, 0.28), (3.83, 0.29)),
+    "ngc7793-w": mean_with_error((3.75, 0.28), (3.83, 0.29)),
+    "ugc1249": (6.4, 0.5),
+    "ugc4305": (3.32, 0.25),
+    "ugc4459": (3.96, 0.30),
+    "ugc5139": (3.83, 0.29),
+    "ugc685": (4.37, 0.34),
+    "ugc695": (7.8, 0.6),
+    "ugc7408": (7.0, 0.5),
+    "ugca281": (5.19, 0.39),
+}
+
+
 def distance(data_path):
-    distances_mpc = {
-        "ngc1313-e": distance_modulus_to_distance_mpc(28.21),  # Jacobs et al. 2009
-        "ngc1313-w": distance_modulus_to_distance_mpc(28.21),  # Jacobs et al. 2009
-        "ngc628-c": distance_modulus_to_distance_mpc(29.98),  # Olivares et al. 2010
-        "ngc628-e": distance_modulus_to_distance_mpc(29.98),  # Olivares et al. 2010
-    }
-    return distances_mpc[data_path.name] * u.Mpc
+    return distances_and_errs_mpc[data_path.name][0] * u.Mpc
 
 
 def distance_error(data_path):
-    distances_error_mpc = {
-        "ngc1313-e": distance_modulus_err_to_distance_err_mpc(28.21, 0.02),
-        "ngc1313-w": distance_modulus_err_to_distance_err_mpc(28.21, 0.02),
-        "ngc628-c": distance_modulus_err_to_distance_err_mpc(29.98, 0.28),
-        "ngc628-e": distance_modulus_err_to_distance_err_mpc(29.98, 0.28),
-    }
-    return distances_error_mpc[data_path.name] * u.Mpc
+    return distances_and_errs_mpc[data_path.name][1] * u.Mpc
 
 
 def arcsec_to_size_pc(arcseconds, home_dir):
