5dc2425b7f88579a1929af4475f031b574de936f
Gillen Brown <gillenb@umich.edu>
2020-07-20 13:42:54 -0400

    copy some plots to shangrila to avoid remaking them

--- copy_to_shangrila.sh
+++ copy_to_shangrila.sh
@@ -1,2 +1,2 @@
 rsync -av /Users/gillenb/code/legus_sizes/plots/* gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/plots/"
-rsync -av --exclude '*drc.fits' --exclude '*sigma_electrons.fits' --exclude '*tar.gz' --exclude '*padagb-mwext-avgapcor*' --exclude '*.png' /Users/gillenb/code/legus_sizes/data/* gillenb@shangrila.astro.lsa.umich.edu:/u/home/gillenb/code/mine/LEGUS-sizes/data/
+rsync -av --exclude '*drc.fits' --exclude '*tar.gz' --exclude '*padagb-mwext-avgapcor*' --exclude '*/cluster_fit_plots/*.png' /Users/gillenb/code/legus_sizes/data/* gillenb@shangrila.astro.lsa.umich.edu:/u/home/gillenb/code/mine/LEGUS-sizes/data/
