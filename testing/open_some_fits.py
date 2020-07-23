bf1317214d26fd1d47a953b8db864f31bec9df33
Gillen Brown <gillenb@umich.edu>
2020-07-23 16:08:26 -0400

    updates to some testing scripts

--- testing/open_some_fits.py
+++ testing/open_some_fits.py
@@ -2,7 +2,9 @@
 open_some_fits.py - this is basically the select PSF stars script, only without the
 buttons to approve or reject. It shows 10 randomly selected clusters per field
 """
-import sys
+import shutil
+
+from astropy import table
 from pathlib import Path
 import random
 
@@ -24,16 +26,30 @@ from PySide2.QtCore import Qt
 # Find the location of the data, and get 10 random plots for each field
 #
 # ======================================================================================
+def criteria(catalog):
+    catalog["check_this"] = catalog["power_law_slope_best"] == 10.0
+
+
 plots_to_show = []
 data_dir = Path("../data").resolve()
 for item in data_dir.iterdir():
     if item.is_dir():
-        fits_dir = item / "size" / "cluster_fit_plots"
+        size_dir = item / "size"
+        fits_dir = size_dir / "cluster_fit_plots"
+        # open the catalog, and get the ones that match our criteria
+        cat_name = "final_catalog_30_pixels_psf_my_stars_15_pixels_2x_oversampled.txt"
+        cat = table.Table.read(size_dir / cat_name, format="ascii.ecsv")
+        criteria(cat)
+
+        ids_to_check = cat["ID"][cat["check_this"]].data
+
         these_plots = []
         for plot in fits_dir.iterdir():
             name = plot.name
             if "30" in name and "best_fit.png" in name:
-                these_plots.append(plot)
+                this_id = int(name.split("_")[1])
+                if this_id in ids_to_check:
+                    these_plots.append(plot)
         # then select a random 10
         if len(these_plots) > 10:
             these_plots = random.sample(these_plots, 10)
@@ -56,6 +72,9 @@ class MainWindow(QMainWindow):
         # shown Below that will be two buttons: previous and next
         vBoxMain = QVBoxLayout()
 
+        self.label = QLabel()
+        vBoxMain.addWidget(self.label)
+
         self.image = QLabel()
         self.image.setScaledContents(True)
         vBoxMain.addWidget(self.image)
@@ -64,18 +83,23 @@ class MainWindow(QMainWindow):
         hBoxInput = QHBoxLayout()
         self.previousButton = QPushButton("Previous")
         self.nextButton = QPushButton("Next")
+        self.copyButton = QPushButton("Copy To Desktop, then Next")
         # set the tasks that each button will do
         self.previousButton.clicked.connect(self.previous)
         self.nextButton.clicked.connect(self.next)
+        self.copyButton.clicked.connect(self.copy)
         # and make keyboard shortcuts
         previousShortcut = QShortcut(QKeySequence("left"), self.previousButton)
         nextShortcut = QShortcut(QKeySequence("right"), self.nextButton)
+        copyShortcut = QShortcut(QKeySequence("c"), self.copyButton)
 
         previousShortcut.activated.connect(self.previous)
         nextShortcut.activated.connect(self.next)
+        copyShortcut.activated.connect(self.copy)
 
         hBoxInput.addWidget(self.previousButton)
         hBoxInput.addWidget(self.nextButton)
+        hBoxInput.addWidget(self.copyButton)
         vBoxMain.addLayout(hBoxInput)
 
         # have to set a dummy widget to act as the central widget
@@ -86,7 +110,7 @@ class MainWindow(QMainWindow):
         # add the first star
         self.showCluster()
 
-        self.resize(1000, 1000)
+        self.resize(800, 800)
 
         # then we can show the widget
         self.show()
@@ -99,12 +123,20 @@ class MainWindow(QMainWindow):
         self.idx -= 1
         self.showCluster()
 
+    def copy(self):
+        shutil.copy2(plots_to_show[self.idx], Path.home() / "Desktop")
+        self.next()
+
     def showCluster(self):
         # add the appropriate image to the GUI
         pixmap = QPixmap(plots_to_show[self.idx])
-        pixmap = pixmap.scaled(1000, 1000, Qt.KeepAspectRatio)
+        pixmap = pixmap.scaled(800, 800, Qt.KeepAspectRatio, Qt.SmoothTransformation)
         self.image.setPixmap(pixmap)
+        self.image.setScaledContents(True)
         self.image.repaint()
+
+        # and adjust the text
+        self.label.setText(Path(plots_to_show[self.idx]).name)
 
 
 app = QApplication()
