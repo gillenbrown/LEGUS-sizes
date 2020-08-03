6688190e55f6b3557b50cd3d0ad8fa9f164eb9b8
Gillen Brown <gillenb@umich.edu>
2020-08-03 14:12:46 -0400

    added goodness of fit indicators to postprocessing

--- pipeline/derived_properties.py
+++ pipeline/derived_properties.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from astropy import table
 from astropy.io import fits
 import numpy as np
+from tqdm import tqdm
 from matplotlib import colors, gridspec
 from matplotlib import pyplot as plt
 import cmocean
@@ -219,7 +220,123 @@ def create_plot_name(id):
     return f"{galaxy_name}_{id:04}_size_{snapshot_size}.png"
 
 
-def plot_model_set(cluster_snapshot, uncertainty_snapshot, mask, params, id):
+def create_radial_profile(model_psf_bin_image, cluster_snapshot, mask, x_c, y_c):
+    """
+    Make a radial profile of cluster and model pixel values
+
+    :param model_psf_bin_image: the model image on the same pixel scale as the data
+    :param cluster_snapshot: the data snapshot
+    :param mask: the mask indicating which pixel values to not use
+    :param x_c: X coordinate of the center in snapshot coordinates
+    :param y_c: Y coordinate of the center in snapshot coordinates
+    :return: Three numpy arrays: The radii of all pixel values, in sorted order, the
+             model values at these radii, then the data values at these radii
+    """
+    # When fitting I treated the center of the pixels as the integer location, so do
+    # that here too
+    radii, model_ys, data_ys = [], [], []
+    for x in range(model_psf_bin_image.shape[1]):
+        for y in range(model_psf_bin_image.shape[0]):
+            if mask[y][x] > 0:
+                radii.append(fit_utils.distance(x, y, x_c, y_c))
+                model_ys.append(model_psf_bin_image[y, x])
+                data_ys.append(cluster_snapshot[y, x])
+
+    # sort everything in order of radii
+    idxs = np.argsort(radii)
+    return np.array(radii)[idxs], np.array(model_ys)[idxs], np.array(data_ys)[idxs]
+
+
+def bin_profile(radii, pixel_values, bin_size):
+    """
+    Take an existing profile and bin it azimuthally
+
+    :param radii: Radii values corresponding to the pixel values
+    :param pixel_values: Values at the radii passed in
+    :param bin_size: How big the bins should be, in pixels
+    :return: Binned radii and pixel values
+    """
+    binned_radii, binned_ys = [], []
+    for r_min in np.arange(0, int(np.ceil(max(radii))), bin_size):
+        r_max = r_min + bin_size
+        idx_above = np.where(r_min < radii)
+        idx_below = np.where(r_max > radii)
+        idx_good = np.intersect1d(idx_above, idx_below)
+
+        if len(idx_good) > 0:
+            binned_radii.append(r_min + 0.5 * bin_size)
+            binned_ys.append(np.mean(pixel_values[idx_good]))
+
+    return np.array(binned_radii), np.array(binned_ys)
+
+
+def rms(sigmas, x_c, y_c, max_radius):
+    """
+    Calculate the RMS of the pixels within some radius
+
+    :param sigmas: Deviations of the pixels from the fit
+    :param x_c: X coordinate of the center, in coordinates of the sigmas snapshot
+    :param y_c: Y coordinate of the center, in coordinates of the sigmas snapshot
+    :param max_radius: Maximum radius to include in the calculation
+    :return: sqrt(mean(sigmas**2)) where r < max_radius
+    """
+    good_sigmas = []
+    for x in range(sigmas.shape[1]):
+        for y in range(sigmas.shape[0]):
+            if fit_utils.distance(x, y, x_c, y_c) < max_radius:
+                good_sigmas.append(sigmas[y, x])
+
+    return np.sqrt(np.mean(np.array(good_sigmas) ** 2))
+
+
+def mad_of_cumulative(radii, model_cumulative, data_cumulative, max_radius):
+    """
+    Calculate the median relative absolute deviation of the cumulative distribution
+    within some radius. This is median(abs(model - data) / data) where r < r_max
+
+    :param radii: List of radii
+    :param model_cumulative: Cumulative pixel values for the model as a function of r
+    :param data_cumulative: Cumulative pixel values for the data as a function of r
+    :param max_radius: Maximum radius to include in the calculation
+    :return: median(abs(model - data) / data) where r < r_max
+    """
+    mask_good = radii < max_radius
+    diffs = np.abs(model_cumulative - data_cumulative) / data_cumulative
+    return np.median(diffs[mask_good])
+
+
+def estimate_background(data, x_c, y_c, min_radius):
+    """
+    Estimate the true background value.
+
+    This will be defined to be the median of all pixels beyond min_radius
+
+    :param data: Values at each pixel
+    :param x_c: X coordinate of the center, in coordinates of the sigmas snapshot
+    :param y_c: Y coordinate of the center, in coordinates of the sigmas snapshot
+    :param min_radius: Minimum radius to include in the calculation
+    :return: median(pixel_value) and std(pixel_value) where r > min_radius,
+    """
+    good_bg = []
+    for x in range(data.shape[1]):
+        for y in range(data.shape[0]):
+            if fit_utils.distance(x, y, x_c, y_c) > min_radius:
+                good_bg.append(data[y, x])
+
+    return np.median(good_bg), np.std(good_bg)
+
+
+def plot_model_set(
+    cluster_snapshot,
+    uncertainty_snapshot,
+    mask,
+    params,
+    id,
+    r_eff,
+    cut_radius,
+    estimated_bg,
+    bg_scatter,
+):
     model_image, model_psf_image, model_psf_bin_image = fit_utils.create_model_image(
         *params, psf, snapshot_size_oversampled, oversampling_factor
     )
@@ -305,41 +422,19 @@ def plot_model_set(cluster_snapshot, uncertainty_snapshot, mask, params, id):
     # the center is in oversampled coords, fix that
     x_c = fit_utils.oversampled_to_image(params[1], oversampling_factor)
     y_c = fit_utils.oversampled_to_image(params[2], oversampling_factor)
-    # When fitting I treated the center of the pixels as the integer location, so do
-    # that here too
-    radii, model_ys, data_ys = [], [], []
-    for x in range(model_psf_bin_image.shape[1]):
-        for y in range(model_psf_bin_image.shape[0]):
-            if mask[y][x] > 0:
-                radii.append(fit_utils.distance(x, y, x_c, y_c))
-                model_ys.append(model_psf_bin_image[y, x])
-                data_ys.append(cluster_snapshot[y, x])
+
+    radii, model_ys, data_ys = create_radial_profile(
+        model_psf_bin_image, cluster_snapshot, mask, x_c, y_c
+    )
 
     ax_pd.scatter(radii, data_ys, c=c_d, s=5, alpha=1.0, label="Data")
     ax_pd.scatter(radii, model_ys, c=c_m, s=5, alpha=1.0, label="Model")
     ax_pd.axhline(0, ls=":", c=bpl.almost_black)
-
-    # convert to numpy array to have nice indexing
-    radii = np.array(radii)
-    model_ys = np.array(model_ys)
-    data_ys = np.array(data_ys)
+    ax_pd.axvline(r_eff, ls=":", c=bpl.almost_black)
 
     # then bin this data to make the binned plot
-    bin_size = 1.0
-    binned_radii, binned_model_ys, binned_data_ys = [], [], []
-    for r_min in np.arange(0, int(np.ceil(max(radii))), bin_size):
-        r_max = r_min + bin_size
-        idx_above = np.where(r_min < radii)
-        idx_below = np.where(r_max > radii)
-        idx_good = np.intersect1d(idx_above, idx_below)
-
-        if len(idx_good) > 0:
-            binned_radii.append(r_min + 0.5 * bin_size)
-            binned_model_ys.append(np.mean(model_ys[idx_good]))
-            binned_data_ys.append(np.mean(data_ys[idx_good]))
-
-    ax_pd.plot(binned_radii, binned_data_ys, c=c_d, lw=5, label="Binned Data")
-    ax_pd.plot(binned_radii, binned_model_ys, c=c_m, lw=5, label="Binned Model")
+    ax_pd.plot(*bin_profile(radii, data_ys, 1.0), c=c_d, lw=5, label="Binned Data")
+    ax_pd.plot(*bin_profile(radii, model_ys, 1.0), c=c_m, lw=5, label="Binned Model")
 
     ax_pd.legend(loc="upper right")
     ax_pd.add_labels("Radius (pixels)", "Background Subtracted Pixel Value [$e^{-}$]")
@@ -352,25 +447,28 @@ def plot_model_set(cluster_snapshot, uncertainty_snapshot, mask, params, id):
     y_max += 0.1 * diff
     ax_pd.set_limits(0, np.ceil(max(radii)), y_min, y_max)
 
-    # then make the cumulative one. get the radii in order
-    idxs_sort = np.argsort(radii)
-    model_ys_cumulative = np.cumsum(model_ys[idxs_sort])
-    data_ys_cumulative = np.cumsum(data_ys[idxs_sort])
+    # then make the cumulative one. The radii are already in order so this is easy
+    model_ys_cumulative = np.cumsum(model_ys)
+    data_ys_cumulative = np.cumsum(data_ys)
 
-    ax_pc.plot(
-        radii[idxs_sort], data_ys_cumulative, c=c_d, label="Data",
-    )
-    ax_pc.plot(
-        radii[idxs_sort], model_ys_cumulative, c=c_m, label="Model",
-    )
+    ax_pc.plot(radii, data_ys_cumulative, c=c_d, label="Data")
+    ax_pc.plot(radii, model_ys_cumulative, c=c_m, label="Model")
     ax_pc.set_limits(0, np.ceil(max(radii)), 0, 1.2 * np.max(data_ys_cumulative))
     ax_pc.legend(loc="upper left")
+    ax_pc.axvline(r_eff, ls=":", c=bpl.almost_black)
     ax_pc.add_labels(
         "Radius (pixels)", "Cumulative Background Subtracted Pixel Values [$e^{-}$]"
     )
 
+    # calculate the relative median devation of cumulative profile and the RMS
+    median_diff = mad_of_cumulative(
+        radii, model_ys_cumulative, data_ys_cumulative, max_radius=cut_radius
+    )
+    this_rms = rms(sigma_image, snapshot_x_cen, snapshot_y_cen, cut_radius)
+
     # the last one just has the list of parameters
     ax_pc.easy_add_text(
+        "$R_{eff}$" + f" = {r_eff:.2f} pixels\n"
         f"log(peak brightness) = {params[0]:.2f}\n"
         f"x center = {fit_utils.oversampled_to_image(params[1], oversampling_factor):.2f}\n"
         f"y center = {fit_utils.oversampled_to_image(params[2], oversampling_factor):.2f}\n"
@@ -378,7 +476,11 @@ def plot_model_set(cluster_snapshot, uncertainty_snapshot, mask, params, id):
         f"q (axis ratio) = {params[4]:.2f}\n"
         f"position angle = {params[5]:.2f}\n"
         f"$\eta$ (power law slope) = {params[6]:.2f}\n"
-        f"background = {params[7]:.2f}\n",
+        f"background = {params[7]:.2f}\n\n"
+        f"cut radius = {cut_radius:.2f} pixels\n"
+        f"estimated background = {estimated_bg:.2f}$\pm${bg_scatter:.2f}\n"
+        f"RMS = {this_rms:,.2f}\n"
+        f"MAD of cumulative profile = {100 * median_diff:.2f}%\n",
         "lower right",
         fontsize=15,
     )
@@ -386,16 +488,23 @@ def plot_model_set(cluster_snapshot, uncertainty_snapshot, mask, params, id):
     fig.savefig(size_dir / "cluster_fit_plots" / create_plot_name(id), dpi=100)
     plt.close(fig)
 
+    return median_diff, this_rms
+
 
 # ======================================================================================
 #
 # Then the actual plotting
 #
 # ======================================================================================
+# add the columns we want to the table
+fits_catalog["profile_mad"] = -99.9
+fits_catalog["estimated_local_background"] = -99.9
+fits_catalog["estimated_local_background_scatter"] = -99.9
+fits_catalog["fit_rms"] = -99.9
 # This is sort of copied from fit.py, as the process of getting the snapshot is
 # basically the same, only here we center on the pixel where the cluster was fitted to
 # be centered
-for row in fits_catalog[:5]:
+for row in tqdm(fits_catalog):
     # create the snapshot. We use ceiling to get the integer pixel values as python
     # indexing does not include the final value. So when we calcualte the offset, it
     # naturally gets biased low. Moving the center up fixes that in the easiest way.
@@ -418,7 +527,7 @@ for row in fits_catalog[:5]:
     snapshot_y_cen = row["y_pix_single_fitted_best"] - y_min
     mask_snapshot = fit_utils.handle_mask(mask_snapshot, snapshot_x_cen, snapshot_y_cen)
 
-    # then plot this up
+    # then get the parameters and calculate a few things of interest
     params = [
         np.log10(row["central_surface_brightness_best"]),
         fit_utils.image_to_oversampled(snapshot_x_cen, oversampling_factor),
@@ -429,7 +538,32 @@ for row in fits_catalog[:5]:
         row["power_law_slope_best"],
         row["local_background_best"],
     ]
-    plot_model_set(data_snapshot, error_snapshot, mask_snapshot, params, row["ID"])
+
+    r_eff = row["r_eff_pixels_rmax_15pix_best"]
+    # Determine the radius within to calculate the fitting quality estimates
+    cut_radius = np.minimum(snapshot_size / 2.0, 3 * r_eff)
+    estimated_bg, bg_scatter = estimate_background(
+        data_snapshot * mask_snapshot, snapshot_x_cen, snapshot_y_cen, cut_radius
+    )
+
+    profile_mad, this_rms = plot_model_set(
+        data_snapshot,
+        error_snapshot,
+        mask_snapshot,
+        params,
+        row["ID"],
+        r_eff,
+        cut_radius,
+        estimated_bg,
+        bg_scatter,
+    )
+
+    row["profile_mad"] = profile_mad
+    row["estimated_local_background"] = estimated_bg
+    row["estimated_local_background_scatter"] = bg_scatter
+    row["fit_rms"] = this_rms
+
+
 # ======================================================================================
 #
 # Then save the table
