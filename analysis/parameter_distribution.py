7aae0c220320042719df4eca40039482a2fb8c60
Gillen Brown <gillenb@umich.edu>
2020-08-06 15:36:09 -0400

    included new oversampled shapshot center in histograms
    Also modified the limits for some parameters

--- analysis/parameter_distribution.py
+++ analysis/parameter_distribution.py
@@ -31,15 +31,15 @@ big_catalog = table.vstack(catalogs, join_type="inner")
 fig, axs = bpl.subplots(ncols=3, nrows=3, figsize=[15, 11])
 axs = axs.flatten()
 
-axs[0].hist(big_catalog["central_surface_brightness_best"], bins=np.logspace(1, 8, 41))
+axs[0].hist(big_catalog["central_surface_brightness_best"], bins=np.logspace(1, 10, 41))
 axs[0].add_labels("Central Surface Brightness [e$^-$]", "Number of Clusters")
-axs[0].set_limits(10, 1e8)
+axs[0].set_limits(10, 1e10)
 axs[0].set_xscale("log")
 
-axs[1].hist(big_catalog["x_pix_single_fitted_best"], bin_size=500)
+axs[1].hist(big_catalog["x_pix_snapshot_oversampled_best"], bin_size=0.25)
 axs[1].add_labels("X Position [pixels]", "Number of Clusters")
 
-axs[2].hist(big_catalog["x_pix_single_fitted_best"], bin_size=500)
+axs[2].hist(big_catalog["y_pix_snapshot_oversampled_best"], bin_size=0.25)
 axs[2].add_labels("Y Position [pixels]", "Number of Clusters")
 
 axs[3].hist(big_catalog["scale_radius_pixels_best"], bins=np.logspace(-10, 5, 41))
@@ -49,7 +49,7 @@ axs[3].set_xscale("log")
 
 axs[4].hist(big_catalog["axis_ratio_best"], bin_size=0.05)
 axs[4].add_labels("Axis ratio", "Number of Clusters")
-axs[4].set_limits(0, 1)
+axs[4].set_limits(0, 1.05)
 
 axs[5].hist(big_catalog["position_angle_best"] % np.pi, bin_size=0.1)
 axs[5].add_labels("Position Angle", "Number of Clusters")
@@ -63,7 +63,7 @@ axs[6].axvline(1, ls=":")
 
 axs[7].hist(big_catalog["local_background_best"], bin_size=100)
 axs[7].add_labels("Local Background", "Number of Clusters")
-axs[7].set_limits(0, 1000)
+axs[7].set_limits(-100, 1000)
 
 axs[8].hist(big_catalog["num_boostrapping_iterations"], bin_size=5)
 axs[8].add_labels("Number of Bootstrapping Iterations", "Number of Clusters")
