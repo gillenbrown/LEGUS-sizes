b35877cb008138594518ed4d642ac80a8d0da808
Gillen Brown <gillenb@umich.edu>
2020-08-07 13:25:12 -0400

    modified copy script to copy all plots
    no separate bootstrapping plots anymore

--- copy_from_shangrila.sh
+++ copy_from_shangrila.sh
@@ -1,4 +1,3 @@
 rsync -av gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/plots/*" /Users/gillenb/code/legus_sizes/plots/
 rsync -av --exclude '*.fits' --exclude '*tar.gz' --exclude '*.png' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/data/*" /Users/gillenb/code/legus_sizes/data/
-#rsync -av --exclude '*drc.fits' --exclude '*sci.fits' --exclude '*tar.gz' --exclude '*bootstrapping*' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/data/*" /Users/gillenb/code/legus_sizes/data/
-#rsync -av --exclude '*drc.fits' --exclude '*sci.fits' --exclude '*tar.gz' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/data/*" /Users/gillenb/code/legus_sizes/data/
+rsync -av --exclude '*drc.fits' --exclude '*sci.fits' --exclude '*tar.gz' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/data/*" /Users/gillenb/code/legus_sizes/data/
