fixed off by one error in pixel values

pipeline/derived_properties.py
@@ -69,8 +69,8 @@ def pad(array, total_length):
 
 
 dist_cols = [
-    "x_pix_single_fitted",
-    "y_pix_single_fitted",
+    "x_fitted",
+    "y_fitted",
     "x_pix_snapshot_oversampled",
     "y_pix_snapshot_oversampled",
     "central_surface_brightness",
@@ -532,8 +532,8 @@ for row in tqdm(fits_catalog):
     # create the snapshot. We use ceiling to get the integer pixel values as python
     # indexing does not include the final value. So when we calcualte the offset, it
     # naturally gets biased low. Moving the center up fixes that in the easiest way.
-    x_cen = int(np.ceil(row["x_pix_single_fitted_best"]))
-    y_cen = int(np.ceil(row["y_pix_single_fitted_best"]))
+    x_cen = int(np.ceil(row["x_fitted_best"]))
+    y_cen = int(np.ceil(row["y_fitted_best"]))
 
     # Get the snapshot, based on the size desired.
     # Since we took the ceil of the center, go more in the negative direction (i.e.
@@ -547,9 +547,12 @@ for row in tqdm(fits_catalog):
     error_snapshot = sigma_data[y_min:y_max, x_min:x_max]
     mask_snapshot = mask_data[y_min:y_max, x_min:x_max]
 
-    snapshot_x_cen = row["x_pix_single_fitted_best"] - x_min
-    snapshot_y_cen = row["y_pix_single_fitted_best"] - y_min
-    mask_snapshot = fit_utils.handle_mask(mask_snapshot, snapshot_x_cen, snapshot_y_cen)
+    snapshot_x_cen = row["x_fitted_best"] - x_min
+    snapshot_y_cen = row["y_fitted_best"] - y_min
+    # Use the same mask region as was used in the actual fitting procedure
+    mask_snapshot = fit_utils.handle_mask(
+        mask_snapshot, row["x"] - x_min, row["y"] - y_min
+    )
 
     # then get the parameters and calculate a few things of interest
     params = [
@@ -580,7 +583,7 @@ for row in tqdm(fits_catalog):
         cut_radius,
         estimated_bg,
         bg_scatter,
-        (row["x_pix_single"] - x_min, row["y_pix_single"] - y_min),
+        (row["x"] - x_min, row["y"] - y_min),
     )
 
     row["profile_mad"] = profile_mad
@@ -591,6 +594,10 @@ for row in tqdm(fits_catalog):
     row["estimated_local_background_diff_sigma"] = diff / bg_scatter
     row["fit_rms"] = this_rms
 
+# Then add one back to the output catalog to be comparable to LEGUS results. This is
+# because of Python being zero indexed
+fits_catalog["x_pix_single_fitted_best"] = fits_catalog["x_fitted_best"] + 1
+fits_catalog["y_pix_single_fitted_best"] = fits_catalog["y_fitted_best"] + 1
 
 # ======================================================================================
 #

pipeline/fit.py
@@ -92,8 +92,8 @@ def dummy_list_col(n_rows):
 
 n_rows = len(clusters_table)
 new_cols = [
-    "x_pix_single_fitted",
-    "y_pix_single_fitted",
+    "x_fitted",
+    "y_fitted",
     "x_pix_snapshot_oversampled",
     "y_pix_snapshot_oversampled",
     "central_surface_brightness",
@@ -427,29 +427,26 @@ def fit_model(data_snapshot, uncertainty_snapshot, mask):
 #
 # ======================================================================================
 for row in tqdm(clusters_table):
-    # create the snapshot. We could use ceiling to get the integer pixel values as
-    # python indexing does not include the final value. So when we calcualte the offset,
-    # it naturally gets biased low. Moving the center up fixes that in the easiest way.
-    # However, the indexing is off by one relative to LEGUS, so taking the floor pushes
-    # it back closer
-    x_cen = int(np.floor(row["x_pix_single"]))
-    y_cen = int(np.floor(row["y_pix_single"]))
+    # create the snapshot. We use ceiling to get the integer pixel values as
+    # python indexing does not include the final value.
+    x_cen = int(np.ceil(row["x"]))
+    y_cen = int(np.ceil(row["y"]))
 
     # Get the snapshot, based on the size desired.
-    # Since we took the floor of the center, go more in the positive direction (i.e.
-    # use floor to get the minimum values). This only matters if the snapshot size is
+    # Since we took the ceil of the center, go more in the negative direction (i.e.
+    # use ceil to get the minimum values). This only matters if the snapshot size is
     # odd
-    x_min = x_cen - int(np.floor(snapshot_size / 2.0))
-    x_max = x_cen + int(np.ceil(snapshot_size / 2.0))
-    y_min = y_cen - int(np.floor(snapshot_size / 2.0))
-    y_max = y_cen + int(np.ceil(snapshot_size / 2.0))
+    x_min = x_cen - int(np.ceil(snapshot_size / 2.0))
+    x_max = x_cen + int(np.floor(snapshot_size / 2.0))
+    y_min = y_cen - int(np.ceil(snapshot_size / 2.0))
+    y_max = y_cen + int(np.floor(snapshot_size / 2.0))
 
     data_snapshot = image_data[y_min:y_max, x_min:x_max]
     error_snapshot = sigma_data[y_min:y_max, x_min:x_max]
     mask_snapshot = mask_data[y_min:y_max, x_min:x_max]
 
     mask_snapshot = fit_utils.handle_mask(
-        mask_snapshot, row["x_pix_single"] - x_min, row["y_pix_single"] - y_min
+        mask_snapshot, row["x"] - x_min, row["y"] - y_min
     )
 
     # then do this fitting!
@@ -461,10 +458,10 @@ for row in tqdm(clusters_table):
     row["central_surface_brightness_best"] = 10 ** results[0]
     row["x_pix_snapshot_oversampled_best"] = results[1]
     row["y_pix_snapshot_oversampled_best"] = results[2]
-    row["x_pix_single_fitted_best"] = x_min + fit_utils.oversampled_to_image(
+    row["x_fitted_best"] = x_min + fit_utils.oversampled_to_image(
         results[1], oversampling_factor
     )
-    row["y_pix_single_fitted_best"] = y_min + fit_utils.oversampled_to_image(
+    row["y_fitted_best"] = y_min + fit_utils.oversampled_to_image(
         results[2], oversampling_factor
     )
     row["scale_radius_pixels_best"] = results[3]
@@ -476,11 +473,11 @@ for row in tqdm(clusters_table):
     row["central_surface_brightness"] = [10 ** v for v in history[0]]
     row["x_pix_snapshot_oversampled"] = history[1]
     row["y_pix_snapshot_oversampled"] = history[2]
-    row["x_pix_single_fitted"] = [
+    row["x_fitted"] = [
         x_min + fit_utils.oversampled_to_image(v, oversampling_factor)
         for v in history[1]
     ]
-    row["y_pix_single_fitted"] = [
+    row["y_fitted"] = [
         y_min + fit_utils.oversampled_to_image(v, oversampling_factor)
         for v in history[2]
     ]

pipeline/make_mask_image.py
@@ -116,8 +116,8 @@ for cluster in tqdm(clusters_table):
     # create the snapshot. We use ceiling to get the integer pixel values as python
     # indexing does not include the final value. So when we calcualte the offset, it
     # naturally gets biased low. Moving the center up fixes that in the easiest way.
-    x_cen = int(np.ceil(cluster["x_pix_single"]))
-    y_cen = int(np.ceil(cluster["y_pix_single"]))
+    x_cen = int(np.ceil(cluster["x"]))
+    y_cen = int(np.ceil(cluster["y"]))
 
     x_min = x_cen - int(np.ceil(snapshot_size / 2.0))
     x_max = x_cen + int(np.floor(snapshot_size / 2.0))
@@ -137,9 +137,7 @@ for cluster in tqdm(clusters_table):
         x = x_idx + 0.5  # to get pixel center
         for y_idx in range(y_min, y_max):
             y = y_idx + 0.5  # to get pixel center
-            dist_cluster = distance(
-                x, y, cluster["x_pix_single"], cluster["y_pix_single"]
-            )
+            dist_cluster = distance(x, y, cluster["x"], cluster["y"])
             # mark the cluster
             if dist_cluster < cluster_mask_radius:
                 mask_data[y_idx][x_idx] = 2