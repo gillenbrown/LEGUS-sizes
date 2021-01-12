ce91bcaabb7cd7bb7b243aa0c18a6e0765cdc865
Gillen Brown <gillenb@umich.edu>
2021-01-12 15:36:50 -0500

    removed M82 SSCs, as they are above 10^5 Msun

--- analysis/mass_radius_relation/mass_radius_legus_mw_external.py
+++ analysis/mass_radius_relation/mass_radius_legus_mw_external.py
@@ -48,17 +48,15 @@ for func, label, color in zip(
         mru_d.get_mw_ymc_krumholz_19_clusters,
         mru_d.get_m31_open_clusters,
         mru_d.get_ngc253_sscs,
-        mru_d.get_m82_sscs,
         mru_d.get_m83_clusters,
     ],
-    ["LEGUS", "MW OCs", "MW YMCs", "M31 OCs", "NGC 253 SSCs", "M82 SSCs", "M83"],
+    ["LEGUS", "MW OCs", "MW YMCs", "M31 OCs", "NGC 253 SSCs", "M83"],
     [
         bpl.color_cycle[0],
         bpl.color_cycle[2],
         bpl.color_cycle[3],
         bpl.color_cycle[4],
         bpl.color_cycle[5],
-        bpl.color_cycle[7],
         bpl.color_cycle[6],
     ],
 ):
@@ -84,8 +82,8 @@ for func, label, color in zip(
     label += f": N = {len(m)}"
 
     # Plot it. Have less common datasets have larger points.
-    # (30 for small datasets, 3 for large)
-    size = 3 + 27 * (len(m) < 20)
+    # (20 for small datasets, 3 for large)
+    size = 3 + 17 * (len(m) < 20)
     mru_p.plot_mass_size_dataset_scatter(
         ax, m_plot, m_el_plot, m_eh_plot, r, r_el, r_eh, color, label, size=size
     )
