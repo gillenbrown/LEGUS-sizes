2f82c97b192f86dc5f3097c89fc4cbf72a440975
Gillen Brown <gillenb@umich.edu>
2021-04-29 15:20:01 -0400

    split density plot by age

--- analysis/density.py
+++ analysis/density.py
@@ -16,6 +16,11 @@ import betterplotlib as bpl
 
 bpl.set_style()
 
+# import a colormap function from the mass-size relation plots
+code_home_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(code_home_dir / "analysis" / "mass_radius_relation"))
+from mass_radius_utils_plotting import create_color_cmap
+
 # ======================================================================================
 #
 # Load the catalogs that were passed in
@@ -28,8 +33,8 @@ catalogs = [table.Table.read(item, format="ascii.ecsv") for item in sys.argv[2:]
 big_catalog = table.vstack(catalogs, join_type="inner")
 
 # restrict to clusters with good masses and radii
-mask = np.logical_and(big_catalog["good_radius"], big_catalog["good_fit"])
-big_catalog = big_catalog[mask]
+good_mask = np.logical_and(big_catalog["good_radius"], big_catalog["good_fit"])
+big_catalog = big_catalog[good_mask]
 
 
 # ======================================================================================
@@ -66,6 +71,19 @@ def kde(x_grid, log_x, log_x_err):
     return ys
 
 
+def contour(ax, mass, r_eff, color, zorder):
+    cmap = create_color_cmap(color, 0.1, 0.8)
+    common = {
+        "percent_levels": [0.5, 0.95],
+        "smoothing": [0.2, 0.2],  # dex
+        "bin_size": 0.05,  # dex
+        "log": True,
+        "cmap": cmap,
+    }
+    ax.density_contourf(mass, r_eff, alpha=0.2, zorder=zorder, **common)
+    ax.density_contour(mass, r_eff, zorder=zorder + 1, **common)
+
+
 # ======================================================================================
 #
 # Get the quantities we'll need for the plot
@@ -88,6 +106,12 @@ mass = big_catalog["mass_msun"]
 m_err_lo = big_catalog["mass_msun"] - big_catalog["mass_msun"]
 m_err_hi = big_catalog["mass_msun_max"] - big_catalog["mass_msun"]
 
+# also set up the masks for age
+age = big_catalog["age_yr"]
+mask_young = age < 1e7
+mask_med = np.logical_and(age >= 1e7, age < 1e8)
+mask_old = np.logical_and(age >= 1e8, age < 1e9)
+
 # ======================================================================================
 #
 # Make the plot
@@ -101,37 +125,52 @@ ax_2_m = axs[1][1]
 
 density_grid = np.logspace(-2, 6, 1000)
 
-ax_3_k.plot(
-    density_grid,
-    kde(density_grid, np.log10(density_3d), density_3d_log_err),
-)
-ax_2_k.plot(
-    density_grid,
-    kde(density_grid, np.log10(density_2d), density_2d_log_err),
-)
+for mask, name, color, zorder in zip(
+    [mask_young, mask_med, mask_old],
+    ["Age: 1-10 Myr", "Age: 10-100 Myr", "Age: 100 Myr - 1 Gyr"],
+    [bpl.color_cycle[0], bpl.color_cycle[5], bpl.color_cycle[3]],
+    [3, 2, 1],
+):
+    ax_3_k.plot(
+        density_grid,
+        kde(density_grid, np.log10(density_3d[mask]), density_3d_log_err[mask]),
+        c=color,
+    )
+    ax_2_k.plot(
+        density_grid,
+        kde(density_grid, np.log10(density_2d[mask]), density_2d_log_err[mask]),
+        c=color,
+        label=name,
+    )
 
-# for the mass-density plots I'll use my contour scatter function. It takes some kwargs,
-# but also has nested dictionaries, so we have to do some nested dicts here too.
-contour = {"log": True}
-common = {
-    "percent_levels": [0.5, 0.75, 0.95],
-    "smoothing": [0.1, 0.1],  # dex
-    "bin_size": 0.05,  # dex
-    "contour_kwargs": {"cmap": "Blues", **contour},
-    "contourf_kwargs": contour,
-    "fill_cmap": "Blues",
-}
-ax_3_m.contour_scatter(mass, density_3d, **common)
-ax_2_m.contour_scatter(mass, density_2d, **common)
+    contour(ax_3_m, mass[mask], density_3d[mask], color, zorder)
+    contour(ax_2_m, mass[mask], density_2d[mask], color, zorder)
+
+    # # for the mass-density plots I'll use my contour scatter function. It takes some kwargs,
+    # # but also has nested dictionaries, so we have to do some nested dicts here too.
+    # contour = {"log": True}
+    # common = {
+    #     "percent_levels":  [0.5, 0.75, 0.95],
+    #     "smoothing":       [0.1, 0.1],  # dex
+    #     "bin_size":        0.05,  # dex
+    #     "contour_kwargs":  {"cmap": "Blues", **contour},
+    #     "contourf_kwargs": contour,
+    #     "fill_cmap":       "Blues",
+    # }
+    # ax_3_m.contour_scatter(mass, density_3d, **common)
+    # ax_2_m.contour_scatter(mass, density_2d, **common)
+
 
 # format axes
-for ax in axs.flatten():
+ax_2_k.legend(loc=2, fontsize=16)
+for ax in axs[0]:
     ax.set_xscale("log")
+    ax.set_limits(0.1, 1e5, 0)
 for ax in axs[1]:
+    ax.set_xscale("log")
     ax.set_yscale("log")
+    ax.set_limits(1e2, 1e6, 0.1, 1e5)
 
-ax_3_k.set_limits(0.1, 1e5, 0)
-ax_2_k.set_limits(0.1, 1e5, 0)
 
 # add labels to the axes
 label_mass = "Mass [$M_\odot$]"
