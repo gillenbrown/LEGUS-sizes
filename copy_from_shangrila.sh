810d0a501598997f49ce01ce2a819e9c310026e5
Gillen Brown <gillenb@umich.edu>
2021-06-01 13:16:15 -0400

    removed scripts to copy data

--- copy_from_shangrila.sh
+++
@@ -1,4 +0,0 @@
-rsync -av gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/outputs_*" /Users/gillenb/code/legus_sizes/
-rsync -av --exclude '*.fits' --exclude '*tar.gz' --exclude '*.png' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/data/*" /Users/gillenb/code/legus_sizes/data/
-rsync -av --exclude '*drc.fits' --exclude '*sci.fits' --exclude '*tar.gz' --exclude '*.png' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/data/*" /Users/gillenb/code/legus_sizes/data/
-rsync -av --exclude '*drc.fits' --exclude '*sci.fits' --exclude '*tar.gz' gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/data/*" /Users/gillenb/code/legus_sizes/data/
--- copy_to_shangrila.sh
+++
@@ -1,2 +0,0 @@
-rsync -av /Users/gillenb/code/legus_sizes/outputs_* gillenb@shangrila.astro.lsa.umich.edu:"/u/home/gillenb/code/mine/LEGUS-sizes/"
-rsync -av --exclude '*drc.fits' --exclude '*tar.gz' --exclude '*padagb-mwext-avgapcor*' --exclude '*/cluster_fit_plots/*.png' /Users/gillenb/code/legus_sizes/data/* gillenb@shangrila.astro.lsa.umich.edu:/u/home/gillenb/code/mine/LEGUS-sizes/data/
